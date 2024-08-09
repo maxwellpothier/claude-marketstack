4be5060add05be9b0de41474ff621a781a3958db
maxwellpothier <pothiermaxwell@gmail.com>
2024-08-09 08:34:09 -0600

    conversion to an api endpoint

--- main.go
+++ main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
+	"net/http"
+	"strings"
 
 	"maxpothier.com/go/api/v2/claude"
 	"maxpothier.com/go/api/v2/marketapi"
 )
 
-var ticker = "AAPL"
-
-func main() {
-	stockResponse, err := marketapi.GetStockData(ticker)
+func getAnalysis(w http.ResponseWriter, r *http.Request) {
+	stockTicker := r.URL.Query().Get("ticker")
+	upperTicker := strings.ToUpper(stockTicker)
+	stockResponse, err := marketapi.GetStockData(upperTicker)
 	if err != nil {
-		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	claudeResponse, err := claude.GetInfoBreakdown(stockResponse)
 	if err != nil {
-		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	fmt.Println(claudeResponse)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(claudeResponse)
+
+}
+
+func main() {
+	server := http.NewServeMux()
+	server.HandleFunc("/analysis", getAnalysis)
+
+	err := http.ListenAndServe(":8080", server)
+	if err != nil {
+		fmt.Println("Error starting server: ", err)
+	}
 }
